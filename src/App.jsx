54346adc52d62e9345e065b5a166bc72963fb358
Abuelyazeed <abuelyazeed175@gmail.com>
2024-06-29 14:20:55 +0300

    changed add projects button styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,21 @@ import { useState } from 'react';
 import ProjectsSideBar from './components/ProjectsSideBar';
 
 function App() {
-  const [projects, setProjects] = useState([
-    {
-      title: 'Learning React',
-      description: '',
-      dueDate: '',
-    },
-    {
-      title: 'Mastering React',
-      description: '',
-      dueDate: '',
-    },
-  ]);
+  // const [projects, setProjects] = useState([
+  //   {
+  //     title: 'Learning React',
+  //     description: '',
+  //     dueDate: '',
+  //   },
+  //   {
+  //     title: 'Mastering React',
+  //     description: '',
+  //     dueDate: '',
+  //   },
+  // ]);
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSideBar projects={projects} />
+      <ProjectsSideBar />
     </main>
   );
 }
--- src/components/ProjectsSideBar.jsx
+++ src/components/ProjectsSideBar.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
 
-function ProjectsSideBar({ projects }) {
+function ProjectsSideBar() {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
         Your Projects
       </h2>
       <div>
-        <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
+        <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
           + Add Project
         </button>
       </div>
+
       <ul className='"p-4 mt-8 rounded-md bg-stone-100"'>
-        {projects.map((p) => {
-          return (
-            <li className="flex justify-between my-4">
-              <button className="w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800">
-                {p.title}
-              </button>
-            </li>
-          );
-        })}
+        <li className="flex justify-between my-4">
+          <button className="w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800">
+            Learning React
+          </button>
+        </li>
+
+        <li className="flex justify-between my-4">
+          <button className="w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800">
+            Mastering React
+          </button>
+        </li>
       </ul>
     </aside>
   );
