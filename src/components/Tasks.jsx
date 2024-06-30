aa685d6713423b303a7f3c14aa74da05e17888e0
Abuelyazeed <abuelyazeed175@gmail.com>
2024-06-30 13:42:49 +0300

    added the newTask component

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
+import NewTask from './NewTask';
 
 function Tasks() {
   return (
     <section>
       <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
-      <p className="flex gap-4 mb-4">
-        <input className="w-64 px-2 py-1 rounded-sm bg-stone-200" />
-        <button className="text-stone-700 hover:text-stone-950">
-          Add Task
-        </button>
-      </p>
-      <ul>Tasks</ul>
-      <p className="text-stone-800 mb-4">
+      <NewTask />
+
+      <p className="text-stone-800 my-4">
         This project does not have any tasks yet.
       </p>
+      <ul></ul>
     </section>
   );
 }
